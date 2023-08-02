d60e34081603147011fe43479a8764d4375cc70f
Тимур Халилов <kotanonum@mail.ru>
2023-08-02 17:52:08 +0300

    Initial commit

---
+++ src/main/java/ru/drill_3x3/Drill_3x3.java
@@ -0,0 +1,17 @@
+package ru.drill_3x3;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Drill_3x3 extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
